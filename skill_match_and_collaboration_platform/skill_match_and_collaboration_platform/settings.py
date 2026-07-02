557b4c6fcf59b407c954f855f26bafc88fd2ab46
ashraf452b <ashraf452b@gmail.com>
2026-07-02 23:55:14 +0600

    update

--- skill_match_and_collaboration_platform/skill_match_and_collaboration_platform/settings.py
+++ skill_match_and_collaboration_platform/skill_match_and_collaboration_platform/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--7-i8fd$rgtwhv#l05y^ud1%bz(b93o_&(-&z&p##18m_1kip8'
-
+# SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
